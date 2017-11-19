7619a1133e518a9f2c6eca46954e2ee660e8d53b
ahtec <gerarddoets@gmail.com>
2017-11-19 14:35:55 +0100

    resizable  digital rain

--- companyPagina.php
+++ companyPagina.php
@@ -22,9 +22,8 @@ require_once './versleutel.php';
                 background-color:#000;
                 display:block;
                 margin:auto;
-                width: 1000px;
-                border: 1px solid green;   
             }
+
             #logo {
                 text-align: center;
                 border: 10px solid black;  
@@ -45,10 +44,10 @@ require_once './versleutel.php';
         </style>
 
     </head>
-    <body>
+    <body onresize="resize_canvas()">
+    <!--<body>-->
             <div id="welkom">
             <!--<img   src=https://www.caict.nl/uploads/nieuws/logo-itph-academy.jpg   width="100% ">--> 
-            <!--<img   src=comp.jpg   hight="100% ">--> 
 
                 <?php
                 $welkomstext = "Welkom ";
@@ -57,27 +56,54 @@ require_once './versleutel.php';
                 $welkomstext .= $_SESSION['ww'];
                 echo "<div id=txt> <h2> $welkomstext </div>";
                 ?>
+                <!--<div  id=logo width="400px;" src='pl.png'> aklsdjhfalkjqwerlok</div>-->
 <!--<img   src=pl.png height="90%" >--> 
 
             </div>
 
-        <!--<canvas id=canvas >-->
+        <canvas id=canvas onclick="resize_canvas()"  >
 
 
 
-<canvas id=canvas width="600px" height="400px"></canvas>
+        <!--<canvas id=canvas width="600px" height="400px"></canvas>-->
 
 
 
-        <!--</canvas>-->
+        </canvas>
 
         <script>
+            
+                function resize_canvas(){
+//                    console.log("In de resize");
+            canvas = document.getElementById("canvas");
+            if (canvas.width  < window.innerWidth)
+            {
+                canvas.width  = window.innerWidth;
+            }
+
+            if (canvas.height < window.innerHeight)
+            {
+                canvas.height = window.innerHeight;
+            }
+            console.log(canvas.height );
+            
+        }
+            
+            
         //        speciaal hier omdat i anders te vroeg laad
             var canvas = document.getElementById("canvas");
-        //            alert(canvas);
+            console.log("Na canvas declared ");
+
+            resize_canvas();
+            
+            console.log(canvas.height );
+            console.log(window.innerHeight);
+            
+//                    alert(canvas);
             var ctx = canvas.getContext("2d");
             var t = text();
-            var logo = document.getElementById('txt');
+//            var logo = document.getElementById('logo');
+//            logo.value =  "$welkomstext";
             var lines = [];
             window.setInterval(draw, 100);
             
@@ -87,11 +113,17 @@ require_once './versleutel.php';
                     lines.push(new textLine());
                 }
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
+                
+                //voor elke regel in de lines arry teken i een img 
+                //met plaatje ti.text op de x coordinaat ti.posx en dat is een methode in textline
+                //tl.animate is een methode die iedere keer een ofset y meegeeft
+
                 lines.forEach(function (tl) {
                     ctx.drawImage(tl.text, tl.posX, tl.animate(), 10, 1000);
                 });
 //                ctx.drawImage(logo, 100, 155, 400, 70);
-            }
+//                ctx.fillText($welkomstext, 10, 50);
+    }
             
             
             function textLine() {
@@ -118,11 +150,11 @@ require_once './versleutel.php';
                 document.body.appendChild(offscreenCanvas);
                 var octx = offscreenCanvas.getContext('2d');
         //            octx.textAlign =center;
-                octx.shadowColor = "lightgreen";
+//                octx.shadowColor = "lightgreen";
 //                octx.shadowOffsetX = 2;
 //                octx.shadowOffsetY = -5;
 //                octx.shadowBlur = 1;
-                octx.fillStyle = 'darkgreen';
+                octx.fillStyle =   'darkgreen';
                 octx.textAlign = "left";
                 var step = 10;
                 for (i = 0; i < 200; i++) {
